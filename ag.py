43905d6352d388a3371646a7ba69be7ea46e0b36
panghaibin <i@hbte.ch>
2022-05-10 16:01:16 +0800

    feat: Antigen up

---
+++ ag.py
@@ -0,0 +1,160 @@
+# -*- coding: UTF-8 -*-
+import os
+import re
+import json
+import base64
+import random
+import logging
+import datetime
+from time import sleep
+from PIL import Image
+from PIL import ImageEnhance
+from utils import abs_path, getUsers, login, html2JsLine, jsLine2Json, getTime, sendMsg, getSendApi
+
+if os.path.exists(abs_path + '/ag.yaml'):
+    config = abs_path + '/ag.yaml'
+else:
+    config = abs_path + '/config.yaml'
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+
+def compress_img(img_path):
+    cps_time = getTime().strftime('%M%S%f')
+    new_img_path = img_path.replace('.jpg', f'_{cps_time}_compress.jpg')
+    target_size = 3 * 1024 * 1024
+    quality = 90
+    step = 5
+    always_compress = False
+
+    cur_img = Image.open(img_path)
+    width, height = cur_img.size
+    crop_width = width * 0.90
+    crop_height = height * 0.90
+    left = random.randint(0, width - crop_width)
+    top = random.randint(0, height - crop_height)
+    right = left + crop_width
+    bottom = top + crop_height
+    cur_img = cur_img.crop((left, top, right, bottom))
+
+    bri_enhancer = ImageEnhance.Brightness(cur_img)
+    cur_img = bri_enhancer.enhance(random.randint(8, 12) / 10)
+    col_enhancer = ImageEnhance.Color(cur_img)
+    cur_img = col_enhancer.enhance(random.randint(9, 11) / 10)
+
+    cur_img.save(new_img_path)
+    cur_img_size = os.path.getsize(new_img_path)
+    while cur_img_size > target_size and quality > 10 or always_compress:
+        cur_img.save(new_img_path, quality=quality, optimize=True)
+        cur_img = Image.open(new_img_path)
+        cur_img_size = os.path.getsize(new_img_path)
+        quality -= step
+        always_compress = False
+
+    return new_img_path
+
+
+def upload_Ag_img(username, password):
+    session = login(username, password)
+    if not session:
+        logging.info('登录失败')
+        return False
+    logging.info('登录成功')
+
+    ag_url = 'https://selfreport.shu.edu.cn/HSJC/HeSJCSelfUploads.aspx'
+    ag_html = session.get(url=ag_url).text
+    logging.info('获取页面成功')
+
+    even_target = 'p1$P_Upload$btnUploadImage'
+    view_state = re.search(r'id="__VIEWSTATE" value="(.*?)" /', ag_html).group(1)
+    view_state_generator = re.search(r'id="__VIEWSTATEGENERATOR" value="(.*?)" /', ag_html).group(1)
+    t = getTime()
+    t -= datetime.timedelta(minutes=2)
+    test_date = t.strftime('%Y-%m-%d %H:%M')
+    test_times = "1" if getTime().hour < 12 else "2"
+
+    id_num = username
+    name = ""
+    ag_line = html2JsLine(ag_html)
+    for i, line in enumerate(ag_line):
+        if 'p1_XingMing' in line:
+            name = jsLine2Json(ag_line[i - 1])['Text']
+            break
+
+    ag_b64str = 'eyJwMV9Hb25nSGFvIjogeyJUZXh0IjogIiJ9LCAicDFfWGluZ01pbmciOiB7IlRleHQiOiAiIn0sICJwMV9QX1VwbG9hZF9DaGVuZ051byI6IHsiQ2hlY2tlZCI6IHRydWV9LCAicDFfUF9VcGxvYWRfY3RsMDAiOiB7IklGcmFtZUF0dHJpYnV0ZXMiOiB7fX0sICJwMV9QX1VwbG9hZF9TaGVuVFpLIjogeyJGX0l0ZW1zIjogW1si5ZCmIiwgIjxzcGFuIHN0eWxlPSdjb2xvcjpncmVlbic+5peg5Lul5LiK55eH54q2KE5vKTwvc3Bhbj4iLCAxXSwgWyLmmK8iLCAiPHNwYW4gc3R5bGU9J2NvbG9yOnJlZCc+5pyJ5Lul5LiK55eH54q25LmL5LiAKFllcyk8L3NwYW4+IiwgMV1dLCAiU2VsZWN0ZWRWYWx1ZSI6ICLlkKYifSwgInAxX1BfVXBsb2FkX0ppYW5DTFgiOiB7IkZfSXRlbXMiOiBbWyLmipfljp8iLCAiPHNwYW4gc3R5bGU9J2ZvbnQtd2VpZ2h0OmJvbGRlcjsnPuaKl+WOnyhBbnRpZ2VuIFRlc3QpPC9zcGFuPiIsIDFdLCBbIuaguOmFuCIsICLmoLjphbgoTnVjbGVpYyBBY2lkIFRlc3QpIiwgMV1dLCAiU2VsZWN0ZWRWYWx1ZSI6ICLmipfljp8ifSwgInAxX1BfVXBsb2FkX0NhaVlGUyI6IHsiRl9JdGVtcyI6IFtbIum8u+iFlOaLreWtkCIsICLpvLvohZTmi63lrZAoTm9zZSkiLCAxXSwgWyLpvLvlkr3mi63lrZAiLCAi6by75ZK95out5a2QKE5vc2UrVGhyb2F0KSIsIDFdLCBbIuWPo+iFlOaLreWtkCIsICLlj6PohZTmi63lrZAoVGhyb2F0KSIsIDFdXSwgIlNlbGVjdGVkVmFsdWUiOiAi6by76IWU5out5a2QIn0sICJwMV9QX1VwbG9hZF9IZVNKQ1JRIjogeyJUZXh0IjogIiJ9LCAicDFfUF9VcGxvYWRfQ2lTaHUiOiB7IkZfSXRlbXMiOiBbWyIxIiwgIuesrDHmrKEoRmlyc3QpIiwgMV0sIFsiMiIsICLnrKwy5qyhKFNlY29uZCkiLCAxXSwgWyIzIiwgIuesrDPmrKEoVGhpcmQpIiwgMV1dLCAiU2VsZWN0ZWRWYWx1ZSI6ICIxIn0sICJwMV9QX1VwbG9hZF9KaWFuQ0pHIjogeyJGX0l0ZW1zIjogW1si6Zi05oCnIiwgIjxzcGFuIHN0eWxlPSdjb2xvcjpncmVlbic+6Zi05oCnKE5lZ2F0aXZlKTwvc3Bhbj4iLCAxXSwgWyLpmLPmgKciLCAiPHNwYW4gc3R5bGU9J2NvbG9yOnJlZCc+6Ziz5oCnKFBvc2l0aXZlKTwvc3Bhbj4iLCAxXSwgWyLml6DmlYgiLCAi5peg5pWIKEludmFsaWQpIiwgMV0sIFsi5pqC5peg57uT5p6cIiwgIuaaguaXoOe7k+aenChObyBSZXN1bHQpIiwgMV1dLCAiU2VsZWN0ZWRWYWx1ZSI6ICLpmLTmgKcifSwgInAxX1BfVXBsb2FkIjogeyJJRnJhbWVBdHRyaWJ1dGVzIjoge319LCAicDFfR3JpZERhdGEiOiB7IlJlY29yZENvdW50IjogMCwgIkZfUm93cyI6IFtdLCAiSUZyYW1lQXR0cmlidXRlcyI6IHt9fSwgInAxIjogeyJJRnJhbWVBdHRyaWJ1dGVzIjoge319LCAiV19TaG93UGljIjogeyJJRnJhbWVBdHRyaWJ1dGVzIjoge319fQ=='
+    ag_json = json.loads(base64.b64decode(ag_b64str).decode('utf-8'))
+    ag_json['p1_GongHao']['Text'] = id_num
+    ag_json['p1_XingMing']['Text'] = name
+    ag_json['p1_P_Upload_HeSJCRQ']['Text'] = test_date
+    ag_json['p1_P_Upload_CiShu']['SelectedValue'] = test_times
+    fstate = base64.b64encode(json.dumps(ag_json, ensure_ascii=False).encode("utf-8")).decode("utf-8")
+
+    report_form = {
+        '__EVENTTARGET': even_target,
+        '__EVENTARGUMENT': '',
+        '__VIEWSTATE': view_state,
+        '__VIEWSTATEGENERATOR': view_state_generator,
+        'p1$P_Upload$ChengNuo': 'p1_P_Upload_ChengNuo',
+        'p1$P_Upload$ShenTZK': b'\xe5\x90\xa6'.decode(),
+        'p1$P_Upload$JianCLX': b'\xe6\x8a\x97\xe5\x8e\x9f'.decode(),
+        'p1$P_Upload$CaiYFS': b'\xe9\xbc\xbb\xe8\x85\x94\xe6\x8b\xad\xe5\xad\x90'.decode(),
+        'p1$P_Upload$HeSJCRQ': test_date,
+        'p1$P_Upload$CiShu': test_times,
+        'p1$P_Upload$JianCJG': b'\xe9\x98\xb4\xe6\x80\xa7'.decode(),
+        'p1_P_Upload_ctl00_Collapsed': 'false',
+        'p1_P_Upload_Collapsed': 'false',
+        'p1_GridData_Collapsed': 'false',
+        'p1_GridData_HiddenColumns': '["p1_GridData_ctl00"]',
+        'p1_Collapsed': 'false',
+        'W_ShowPic_Collapsed': 'false',
+        'W_ShowPic_Hidden': 'true',
+        'F_STATE': fstate,
+        'F_TARGET': 'p1_P_Upload_btnUploadImage',
+        'X-FineUI-Ajax': 'true',
+    }
+    img_list = os.listdir(abs_path + '/ag_img/')
+    img_path = random.choice(img_list)
+    img_path = abs_path + '/ag_img/' + img_path
+    img_path = compress_img(img_path)
+    img = open(img_path, 'rb')
+    file = {
+        'p1$P_Upload$FileHeSJCBG': (f'{id_num}.jpg', img, 'image/jpeg', {'Content-Type': 'image/jpeg'}),
+    }
+    ag_upload = 'https://selfreport.shu.edu.cn/HSJC/HeSJCSelfUploads.aspx'
+    result = session.post(url=ag_upload, data=report_form, files=file).text
+
+    send_api = getSendApi(config)
+    title = f'{id_num[-3:]}的第{test_times}次结果'
+    now = t.strftime('%Y-%m-%d %H:%M:%S')
+    if '上传成功' in result:
+        logging.info(f'{id_num[-3:]}上传成功')
+        title += '上传成功'
+        desp = now + '\n\n' + title
+        send_result = sendMsg(title, desp, send_api['api'], send_api['key'])
+    else:
+        logging.info(f'{id_num[-3:]}上传失败')
+        result = result.split('F.alert')[-1]
+        result = result.split('&#39;')[1]
+        logging.info(result)
+        title += '上传失败'
+        desp = now + '\n\n' + title + '\n\n' + result
+        send_result = sendMsg(title, desp, send_api['api'], send_api['key'])
+    logging.info('消息发送成功') if send_result else logging.info('消息发送失败')
+    img.close()
+    os.remove(img_path)
+
+
+def main():
+    users = getUsers(config)
+    for i, j in enumerate(users.items()):
+        username, password = j
+        password = password[0]
+        upload_Ag_img(username, password)
+        if i < len(users) - 1:
+            sleep(5 * 60)
+
+
+if __name__ == '__main__':
+    if main():
+        exit(0)
+    else:
+        exit(1)
